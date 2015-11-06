4b74f6a86b76b982006eedaf7253b017b9eecd08
Yingding Wang <yingdingwang@yahoo.de>
2015-11-06 17:25:44 +0100

    update docs

--- idsAutoUpdater1/run_ids_upgrader.sh
+++ idsAutoUpdater1/run_ids_upgrader.sh
@@ -6,6 +6,6 @@ sh 02_install.sh;
 sh 03_postconfigIDSDB.sh;
 sh 04_upgradingIDSDB.sh
 
-echo "Upgrading IDS lmu done"
+echo "Upgrading IDS done"
 echo "thanks for you advice to improve the migrator"
 
